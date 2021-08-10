4f7b3200851d6d6c7d180cb5952361b84aaf7fc5
Matt W <mattworby@gmail.com>
2021-08-10 17:46:13 -0500

    Added Video Game API
    Added an API from Twitch with video games. Added radio button to alternate between the two

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const Hapi = require('hapi');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const axios = require('axios').default;
 
 const serverOptions = {
     host: 'localhost',
@@ -17,14 +19,20 @@ const server = new Hapi.Server(serverOptions);
   // Handle a new viewer requesting the color.
   server.route({
     method: 'GET',
-    path: '/{type}/{searchItem}/{page}',
+    path: '/movie/{searchItem}/{page}',
     handler: handleHapiRequest
   });
 
   server.route({
     method: 'GET',
-    path: '/{type}/{id}',
+    path: '/movie/{id}',
     handler: handleMovieRequest
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/game/{gameTitle}',
+    handler: handleVideoGame
   });
 
   // Start the server.
@@ -37,6 +45,8 @@ process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
+
+//********************************************************** movies **********************************************************/
 
 async function handleHapiRequest(req){
     let movieSearch = req.params.searchItem;;
@@ -136,4 +146,77 @@ function getMovieById(movieId,callback){
     }).on("error", (error) => {
         console.error(error.message);
     });
+}
+
+//********************************************************** video games *********************************************************/
+
+async function handleVideoGame(req){
+    let game = req.params.gameTitle;
+    let getGame;
+
+    game = game.replace(/ /g,"+");
+
+    
+    let promise = new Promise((resolve,reject) =>{
+		getVideoGames(game,function(result){
+			resolve(result);
+		});
+	});
+
+    getGame = await promise;
+
+    return getGame;
+
+}
+
+function getVideoGames(game, callback){
+    let url = "https://id.twitch.tv/oauth2/token";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = async function () {
+       if (xhr.readyState === 4) {
+          let json = JSON.parse(xhr.responseText);
+          let getPromise;
+
+            let promise = new Promise((resolve,reject) =>{
+                retrieveVideoGame(json.access_token,game,function(result){
+                    resolve(result);
+                });
+            });
+
+            getPromise = await promise;
+
+            return callback(getPromise);
+       }};
+
+    let data = `{
+      	"client_id":"j8gsbdu9iqblx4ian3gh6lxhxs3djc",
+    	"client_secret":"ojq3syke1u94pygd3n2f67vzoc5uoq",
+    	"grant_type":"client_credentials"
+    }`;
+
+    xhr.send(data);
+}
+
+function retrieveVideoGame(token,game,callback){
+    axios({
+        url: "https://api.igdb.com/v4/games",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Client-ID': 'j8gsbdu9iqblx4ian3gh6lxhxs3djc',
+            'Authorization': `Bearer ${token}`
+        },
+        data: `fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; search \"${game}\"; `
+      })
+        .then(response => {
+            return callback(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,52 +1,33 @@
 let pageNumber;
 let searchTerm;
 
-function populateSearch(resOne,resTwo){
-    let divCount = 0;
+//**********************Handle Search************************ */
 
-    document.getElementById('searchReturnDiv').innerHTML = '';
+function handleSearch(searchValue){
+    let radioSearch = document.getElementsByName('searchRadio');
 
-    for(let i = 0; i < resOne.length; i++){
-        if (i % 5 == 0){
-            divCount += 1;
-            document.getElementById('searchReturnDiv').innerHTML += `<div id='flexDiv${divCount}'></div>`;
-            document.getElementById(`flexDiv${divCount}`).style.display = 'flex';
-            document.getElementById(`flexDiv${divCount}`).style.height = '100%';
-            document.getElementById(`flexDiv${divCount}`).style.marginTop = '1vh';
-            document.getElementById(`flexDiv${divCount}`).style.marginRight = '2vw';
+    for (let i = 0; i < radioSearch.length; i++){
+        if(radioSearch[i].checked){
+            switch (radioSearch[i].value){
+                case 'MOVIE':
+                    beginMovieSearch(searchValue);
+                    break;
+                case 'GAME':
+                    getVideoGames(searchValue);
+                    break;
+            }
+            break;
         }
-        document.getElementById(`flexDiv${divCount}`).innerHTML += `<div id='${resOne[i].imdbID}${i}'></div>`
-        document.getElementById(`${resOne[i].imdbID}${i}`).style.backgroundImage = `url(${resOne[i].Poster})`
-        document.getElementById(`${resOne[i].imdbID}${i}`).style.height = '100%';
-        document.getElementById(`${resOne[i].imdbID}${i}`).style.flex = '1';
-        document.getElementById(`${resOne[i].imdbID}${i}`).style.backgroundRepeat = 'no-repeat';
-        document.getElementById(`${resOne[i].imdbID}${i}`).style.backgroundSize = 'contain';
-        document.getElementById(`${resOne[i].imdbID}${i}`).setAttribute('onclick',`openMovie("${resOne[i].imdbID}")`);
-    }
-    for(let i = 0; i < resTwo.length; i++){
-        if (i % 5 == 0){
-            divCount += 1;
-            document.getElementById('searchReturnDiv').innerHTML += `<div id='flexDiv${divCount}'></div>`;
-            document.getElementById(`flexDiv${divCount}`).style.display = 'flex';
-            document.getElementById(`flexDiv${divCount}`).style.height = '100%';
-            document.getElementById(`flexDiv${divCount}`).style.marginTop = '1vh';
-            document.getElementById(`flexDiv${divCount}`).style.marginRight = '2vw';
-        }
-        document.getElementById(`flexDiv${divCount}`).innerHTML += `<div id='${resTwo[i].imdbID}${i}'></div>`
-        document.getElementById(`${resTwo[i].imdbID}${i}`).style.backgroundImage = `url(${resTwo[i].Poster})`
-        document.getElementById(`${resTwo[i].imdbID}${i}`).style.height = '100%';
-        document.getElementById(`${resTwo[i].imdbID}${i}`).style.flex = '1';
-        document.getElementById(`${resTwo[i].imdbID}${i}`).style.backgroundRepeat = 'no-repeat';
-        document.getElementById(`${resTwo[i].imdbID}${i}`).style.backgroundSize = 'contain';
-        document.getElementById(`${resOne[i].imdbID}${i}`).setAttribute('onclick',`openMovie("${resOne[i].imdbID}")`);
     }
 }
 
-function beginSearch(value){
+//**********************Movies************************ */
+
+function beginMovieSearch(value){
     pageNumber = 1;
     searchTerm = value;
 
-    fetch(`http://localhost:8082/search/${searchTerm}/${pageNumber}`).then(function (response) {
+    fetch(`http://localhost:8082/movie/${searchTerm}/${pageNumber}`).then(function (response) {
 	    // The API call was successful!
 	    return response.json();
     }).then(function (data) {
@@ -95,8 +76,69 @@ function nextSearch(dir){
     });
 }
 
+function populateSearch(resOne,resTwo){
+    let divCount = 0;
+
+    document.getElementById('searchReturnDiv').innerHTML = '';
+
+    for(let i = 0; i < resOne.length; i++){
+        if (i % 5 == 0){
+            divCount += 1;
+            document.getElementById('searchReturnDiv').innerHTML += `<div id='flexDiv${divCount}'></div>`;
+            document.getElementById(`flexDiv${divCount}`).style.display = 'flex';
+            document.getElementById(`flexDiv${divCount}`).style.height = '100%';
+            document.getElementById(`flexDiv${divCount}`).style.marginTop = '1vh';
+            document.getElementById(`flexDiv${divCount}`).style.marginRight = '2vw';
+        }
+        document.getElementById(`flexDiv${divCount}`).innerHTML += `<div id='${resOne[i].imdbID}${i}'></div>`
+        document.getElementById(`${resOne[i].imdbID}${i}`).style.backgroundImage = `url(${resOne[i].Poster})`
+        document.getElementById(`${resOne[i].imdbID}${i}`).style.height = '100%';
+        document.getElementById(`${resOne[i].imdbID}${i}`).style.flex = '1';
+        document.getElementById(`${resOne[i].imdbID}${i}`).style.backgroundRepeat = 'no-repeat';
+        document.getElementById(`${resOne[i].imdbID}${i}`).style.backgroundSize = 'contain';
+        document.getElementById(`${resOne[i].imdbID}${i}`).setAttribute('onclick',`openMovie("${resOne[i].imdbID}")`);
+    }
+    for(let i = 0; i < resTwo.length; i++){
+        if (i % 5 == 0){
+            divCount += 1;
+            document.getElementById('searchReturnDiv').innerHTML += `<div id='flexDiv${divCount}'></div>`;
+            document.getElementById(`flexDiv${divCount}`).style.display = 'flex';
+            document.getElementById(`flexDiv${divCount}`).style.height = '100%';
+            document.getElementById(`flexDiv${divCount}`).style.marginTop = '1vh';
+            document.getElementById(`flexDiv${divCount}`).style.marginRight = '2vw';
+        }
+        document.getElementById(`flexDiv${divCount}`).innerHTML += `<div id='${resTwo[i].imdbID}${i}'></div>`
+        document.getElementById(`${resTwo[i].imdbID}${i}`).style.backgroundImage = `url(${resTwo[i].Poster})`
+        document.getElementById(`${resTwo[i].imdbID}${i}`).style.height = '100%';
+        document.getElementById(`${resTwo[i].imdbID}${i}`).style.flex = '1';
+        document.getElementById(`${resTwo[i].imdbID}${i}`).style.backgroundRepeat = 'no-repeat';
+        document.getElementById(`${resTwo[i].imdbID}${i}`).style.backgroundSize = 'contain';
+        document.getElementById(`${resOne[i].imdbID}${i}`).setAttribute('onclick',`openMovie("${resOne[i].imdbID}")`);
+    }
+}
+
 function openMovie (id){
     var para = new URLSearchParams();
     para.append("id", id);
     location.href = "moviePage.html?" + para.toString();
+}
+
+//********************** Video Games************************ */
+
+function getVideoGames(search){
+    pageNumber = 1;
+    searchTerm = search;
+
+    fetch(`http://localhost:8082/game/${searchTerm}`).then(function (response) {
+	    // The API call was successful!
+	    return response.json();
+    }).then(function (data) {
+	    // This is the JSON from our response
+        console.log(data);
+    }).catch(function (err) {
+	    // There was an error
+	    console.warn('Something went wrong.', err);
+    });
+
+    document.getElementById('nextSearchButton').disabled = false;
 }
